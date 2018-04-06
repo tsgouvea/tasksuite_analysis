99612aea62742e609d8bce9e5e31f67fc871d74a
Thiago Gouvêa <tsgouvea@gmail.com>
2018-04-06 11:20:45 -0400

    Module organized by task first. Replaced examples with real dataset files. Main script is a notebook. Added requirements file.

---
+++ tasks/discrate.py
@@ -0,0 +1,172 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mp
+import matplotlib.pyplot as plt
+
+class parser:
+
+    def __init__(self,struct):
+        self.bpod = struct['SessionData']
+        self.params = pd.Series({n: np.asscalar(self.bpod['Settings'].item()['GUI'].item()[n]) for n in self.bpod['Settings'].item()['GUI'].item().dtype.names})
+        self.parse()
+
+    def parse(self):
+        nTrials = np.arange(np.asscalar(self.bpod['nTrials']))+1
+        tsState0 = self.bpod['TrialStartTimestamp'].item()
+        ChoiceLeft = np.full(len(nTrials),False)
+        ChoiceRight = np.full(len(nTrials),False)
+        Rewarded = np.full(len(nTrials),False)
+        Forced = np.full(len(nTrials),False)
+        tsTrialStart = np.full(len(nTrials),np.nan)
+        tsChoice = np.full(len(nTrials),np.nan)
+        tsRwdTone = np.full(len(nTrials),np.nan)
+        tsRwd = np.full(len(nTrials),np.nan)
+        tsPokeL = [[]]*len(nTrials)
+        tsPokeC = [[]]*len(nTrials)
+        tsPokeR = [[]]*len(nTrials)
+        delayPre = np.full(len(nTrials),np.nan)
+        delayPost = np.full(len(nTrials),np.nan)
+
+        for i in range(len(nTrials)) :
+            listStates = self.bpod['RawData'].item()['OriginalStateNamesByNumber'].item()[i]
+
+            try:
+                stateTraj = listStates[self.bpod['RawData'].item()['OriginalStateData'].item()[i]-1]
+            except Exception as e:
+                print(i)
+                print(type(i))
+                print([self.bpod['RawData'].item()['OriginalStateData'].item()[i]])
+
+            #stateTraj = listStates[self.bpod['RawData'].item()['OriginalStateData'].item()[i]-1]
+            ChoiceLeft[i] = any(['PreL' in stateTraj])
+            ChoiceRight[i] = any(['PreR' in stateTraj])
+            Rewarded[i] = any([n.startswith('water_') for n in stateTraj])
+            Forced[i] = any([n.startswith('forc_') for n in stateTraj])
+            tsTrialStart[i] = self.bpod['RawEvents'].item()['Trial'].item()[i]['States'].item()['wait_Cin'].item()[1]
+            tsChoice[i] = self.bpod['RawEvents'].item()['Trial'].item()[i]['States'].item()[stateTraj[[n.startswith('Pre') for n in stateTraj]].item()].item()[0]
+            ndxTone = np.logical_or(np.logical_or([n == 'Wait_Lin' for n in stateTraj],[n == 'Wait_Rin' for n in stateTraj]),[n.startswith('rewcue_') for n in stateTraj])
+            tsRwdTone[i] = self.bpod['RawEvents'].item()['Trial'].item()[i]['States'].item()[stateTraj[ndxTone].item()].item()[0]
+            tsRwd[i] = self.bpod['RawEvents'].item()['Trial'].item()[i]['States'].item()[stateTraj[[n.startswith('rewarded_') for n in stateTraj]].item()].item()[0]
+            PortL = 'Port' + str(int(self.params.Ports_LMR))[0] + 'In'
+            if any([PortL in self.bpod['RawEvents'].item()['Trial'].item()[i]['Events'].item().dtype.names]) :
+                tsPokeL[i] = self.bpod['RawEvents'].item()['Trial'].item()[i]['Events'].item()[PortL].item()
+            PortC = 'Port' + str(int(self.params.Ports_LMR))[1] + 'In'
+            if any([PortC in self.bpod['RawEvents'].item()['Trial'].item()[i]['Events'].item().dtype.names]) :
+                tsPokeC[i] = self.bpod['RawEvents'].item()['Trial'].item()[i]['Events'].item()[PortC].item()
+            PortR = 'Port' + str(int(self.params.Ports_LMR))[2] + 'In'
+            if any([PortR in self.bpod['RawEvents'].item()['Trial'].item()[i]['Events'].item().dtype.names]) :
+                tsPokeR[i] = self.bpod['RawEvents'].item()['Trial'].item()[i]['Events'].item()[PortR].item()
+            delayPost[i] = np.diff(self.bpod['RawEvents'].item()['Trial'].item()[i]['States'].item()[stateTraj[-1]].item())[0]
+        delayPre = tsRwdTone - tsChoice
+
+        assert all(np.logical_xor(ChoiceRight,ChoiceLeft))
+
+        self.parsedData = pd.DataFrame({'nTrials': nTrials, 'ChoiceLeft': ChoiceLeft, 'Rewarded': Rewarded, 'Forced': Forced,
+                                        'tsTrialStart': tsTrialStart, 'tsChoice': tsChoice, 'tsRwdTone': tsRwdTone, 'tsRwd': tsRwd,
+                                        'tsPokeL': tsPokeL, 'tsPokeC': tsPokeC, 'tsPokeR': tsPokeR, 'tsState0': tsState0,
+                                        'delayPre': delayPre, 'delayPost': delayPost})
+
+class dailyfig:
+
+    def __init__(self,sessData):
+
+        mp.rc('xtick', labelsize=5)
+        mp.rc('ytick', labelsize=5)
+
+        data = sessData.parsedData
+        plt.subplots(3,4)
+
+        ndxChoL = data.ChoiceLeft.values
+        ndxChoR = np.logical_not(ndxChoL)
+        ndxForc = data.Forced.values
+        ndxRwd = data.Rewarded.values
+
+        hrate = plt.subplot(341)
+        hrate.set_xlabel('Time (min) from session start', fontsize=7)
+        hrate.set_ylabel('Cumulative trial count', fontsize=7)
+        plt.plot(np.asarray(data.tsState0.values[ndxChoL]-data.tsState0.values[0])/60,np.arange(np.sum(ndxChoL)), color='xkcd:mango')
+        plt.plot(np.asarray(data.tsState0.values[ndxChoR]-data.tsState0.values[0])/60,np.arange(np.sum(ndxChoR)), color='xkcd:darkish green')
+
+        plt.subplot(342)
+        plt.plot(data.tsState0.values[np.logical_and(ndxChoL,ndxForc)]-data.tsState0.values[0],np.arange(np.sum(np.logical_and(ndxChoL,ndxForc))))
+        plt.plot(data.tsState0.values[np.logical_and(ndxChoL,ndxForc)]-data.tsState0.values[0],np.arange(np.sum(np.logical_and(ndxChoL,ndxForc))))
+
+        hrSt0 = plt.subplot(345)
+        hhSt0 = plt.subplot(349)
+        aliSt0 = np.zeros(data.tsState0.shape)
+        self.raspsth(data,aliSt0,(hrSt0,hhSt0))
+        hhSt0.set_xlabel('Time (s) from state0', fontsize=7)
+        hhSt0.set_ylabel('Trial count', fontsize=7)
+
+        hrCin = plt.subplot(346)
+        hhCin = plt.subplot(3,4,10)
+        aliCin = data.tsTrialStart
+        self.raspsth(data,aliCin,(hrCin,hhCin))
+        hhCin.set_xlabel('Time (s) from Cin', fontsize=7)
+
+        hrRtn = plt.subplot(347)
+        hhRtn = plt.subplot(3,4,11)
+        aliRtn = data.tsRwdTone
+        self.raspsth(data,aliRtn,(hrRtn,hhRtn))
+        hhRtn.set_xlabel('Time (s) from RwdTone', fontsize=7)
+
+        hrRwd = plt.subplot(348)
+        hhRwd = plt.subplot(3,4,12)
+        aliRwd = data.tsRwd
+        self.raspsth(data,aliRwd,(hrRwd,hhRwd))
+        hhRwd.set_xlabel('Time (s) from Rwd', fontsize=7)
+
+        plt.tight_layout()
+
+        self.fig = plt.gcf()
+
+    def raspsth(self,data,alignment,panes):
+
+        hisPkL = []
+        hisPkR = []
+        hisPkC = []
+        hisTone = []
+        hisRwd = []
+
+        haRaster, haHist = panes
+
+        iRow = 0;
+        for i in np.argsort(data.ChoiceLeft).values :
+            haRaster.eventplot([data.tsPokeL[i]-alignment[i]], lineoffsets=iRow, colors='xkcd:mango')
+            haRaster.eventplot([data.tsPokeR[i]-alignment[i]], lineoffsets=iRow, colors='xkcd:darkish green')
+            haRaster.eventplot([data.tsPokeC[i]-alignment[i]], lineoffsets=iRow, colors='xkcd:scarlet')
+            haRaster.eventplot([data.tsRwdTone[i]-alignment[i]], lineoffsets=iRow, colors='xkcd:charcoal')
+            haRaster.eventplot([data.tsRwd[i]-alignment[i]], lineoffsets=iRow, colors='xkcd:water blue')
+            iRow += 1
+
+            hisPkL = np.hstack((hisPkL,data.tsPokeL[i]-alignment[i]))
+            hisPkR = np.hstack((hisPkR,data.tsPokeR[i]-alignment[i]))
+            hisPkC = np.hstack((hisPkC,data.tsPokeC[i]-alignment[i]))
+            hisTone = np.hstack((hisTone,data.tsRwdTone[i]-alignment[i]))
+            hisRwd = np.hstack((hisRwd,data.tsRwd[i]-alignment[i]))
+
+        haHist.hist(hisPkL, bins='auto', histtype='step', color='xkcd:mango', density=False)
+        haHist.hist(hisPkR, bins='auto', histtype='step', color='xkcd:darkish green', density=False)
+        haHist.hist(hisPkC, bins='auto', histtype='step', color='xkcd:scarlet', density=False)
+        haHist.hist(hisTone, histtype='step', color='xkcd:charcoal', density=False)
+        haHist.hist(hisRwd, histtype='step', color='xkcd:water blue', density=False)
+
+class multisess:
+
+    def __init__(self):
+        self.summary = pd.DataFrame({'preL': [], 'preR': [], 'posL': [], 'posR': [], 'pLeft': [], 'logOdds': []})
+
+    def append(self,parserOutput):
+        if parserOutput.params.LeftA == 1 :
+            preL = parserOutput.params.PreA
+            posL = parserOutput.params.PostA
+            preR = parserOutput.params.PreB
+            posR = parserOutput.params.PostB
+        elif parserOutput.params.LeftA == 0 :
+            preL = parserOutput.params.PreB
+            posL = parserOutput.params.PostB
+            preR = parserOutput.params.PreA
+            posR = parserOutput.params.PostA
+        pLeft = np.mean(parserOutput.parsedData.ChoiceLeft.values)
+        logOdds = np.log(pLeft/(1-pLeft))
+        self.summary = self.summary.append({'preL': preL, 'preR': preR, 'posL': posL, 'posR': posR, 'pLeft': pLeft, 'logOdds': logOdds}, ignore_index=True)
