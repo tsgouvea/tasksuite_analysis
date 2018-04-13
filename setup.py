999e7fb70107e96701e9054306d33bdb9394ab6a
Thiago Gouvêa <tsgouvea@gmail.com>
2018-04-13 14:39:34 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tasksuite',
+    version='0.0.dev0'
+    description='Analyses data of Bpod behavioral tasks'
+    long_description_content_type='text/markdown'
+    url='https://bitbucket.org/tsgouvea/tasksuite/'
+    author='Thiago S. Gouvea'
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Private / Kepecs lab',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='sample setuptools development',  # Optional
+    packages=find_packages(),  # Required
+
+    project_urls={  # Optional
+        'Source': 'https://bitbucket.org/tsgouvea/tasksuite',
+    },
+)
